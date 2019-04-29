4ad3ff890c3c16104aca4e7496a56891966c54c8
luismetzger <luismetzger@me.com>
2019-04-29 17:19:36 -0600

    Add spec, controller, view for new form.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'index' do
+  context 'index' do
     it 'can be reached sucessfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
@@ -10,6 +10,24 @@ describe 'navigate' do
     it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+  end
+
+  context 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
     end
   end
 end
