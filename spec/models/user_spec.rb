d305d9c1e74b6777144146b0a3f0808ba57e0c1b
luismetzger <luismetzger@me.com>
2019-04-29 16:33:34 -0600

    Add speec and model for Posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:user) { User.create(email: "test@example.com", first_name: "John", last_name: "Doe", password: "asdf123", password_confirmation: "asdf123") }
-  describe 'creation' do
+  describe 'Creation' do
     it 'can be created' do
       expect(user).to be_valid
     end
