ad9bbfccdff5e0133f5ef4f7570626044cb65b71
luismetzger <luismetzger@me.com>
2019-04-30 18:58:20 -0600

    Refactor all posts. Update post model spec.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let!(:post) { Post.create(date: Date.today, rationale: "Anything") }
+  let!(:user) { User.create(email: "test@example.com", first_name: "John", last_name: "Doe", password: "asdf123", password_confirmation: "asdf123") }
+  let!(:post) { Post.create(date: Date.today, rationale: "Anything", user_id: user.id) }
 
   describe 'Creation' do
     it 'can be created' do
